2b1467e8f420e2e96ef457cf399b6f966c7a65bb
jrietema <jan.rietema@web.de>
2015-01-20 12:44:43 +0100

    ensure navigation of top (window) frame in navigation helper

--- app/helpers/simply_pages/navigation_helper.rb
+++ app/helpers/simply_pages/navigation_helper.rb
@@ -10,7 +10,7 @@ module SimplyPages
         page_path = main_app.simply_pages_render_path(slug: page.slug)
         active_class = (request.path == page_path) ? SimplyPages.navigation_active_item_class : nil
         css_class = [SimplyPages.navigation_item_class, active_class].compact.join(' ').sub(/^\s+/,'')
-        html << content_tag(tag_type, link_to(page.title, page_path), class: css_class)
+        html << content_tag(tag_type, link_to(page.title, page_path, target: '_top'), class: css_class)
       end
       html.html_safe
     end
